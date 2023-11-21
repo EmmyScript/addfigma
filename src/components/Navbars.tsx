e47dd3b9b8ab66a0c0b5ea3751cce3f98c463e77
Idoko Emmanuel <idokoemma.ogbole@gmail.com>
2023-11-21 01:39:59 +0100

    addfiga

--- src/components/Navbars.tsx
+++ src/components/Navbars.tsx
@@ -1,6 +1,6 @@
 import {Button,  Nav, Row, Stack  } from "react-bootstrap"
-import { FaFacebook, FaInstagram, FaPinterest, FaShoppingBasket, FaTelegram, FaTwitter, FaUber, FaWhatsapp,
- FaUser, FaGem, FaAdjust,FaCompressArrowsAlt, FaHotel} from "react-icons/fa"
+import { FaFacebook, FaInstagram, FaPinterest, FaTelegram, FaTwitter, FaUber, FaWhatsapp,
+ FaUser, FaAdjust,FaCompressArrowsAlt, FaHotel} from "react-icons/fa"
  import{FaUsersGear,FaSatelliteDish } from "react-icons/fa6"
  import{MdOutlinePhoneCallback, MdShoppingCart, MdOutlineAddIcCall} from "react-icons/md";
 import { Link } from "react-router-dom"
@@ -13,17 +13,14 @@ const Navbars= () => {
 <div className="container-fluiid " >
     <Row  className =" nav-item" >
 
-
-    <nav className="navbar sticky-top all-nav p-3">
+<div className=" all-nav p-3 col-md-4">
+<a className="  electronics navbar-brand text-white" href="#"><FaSatelliteDish />Ma Interested in the RECYCYLING JOB @oshodi</a>
   
-    <a className=" electronics navbar-brand" href="#"><FaSatelliteDish /></a>
+</div>
+<div className="all-nav p-3 col-md-8">
   
-
-    <Nav className ="me-auto">
-<h2 className="electronics"></h2>
-    
-</Nav>
-<Stack className="electronics" direction="horizontal" gap={4}>
+<Nav className="  nav-top d-flex">
+  <Stack direction="horizontal" gap={2}>
     <h2>Follow us:</h2>
 <span><FaWhatsapp/></span>
 <span><FaFacebook/></span>
@@ -33,13 +30,12 @@ const Navbars= () => {
 <span><FaUber/></span>
 
 <span><FaInstagram/></span>
+</Stack>
 
 
-
-        <div className="  dropdown  d-flex ">
-    <a className="nav-link ">
-            ENG:
-          </a>
+        <div className=" dropdown  d-flex ">
+    <h1>Eng:</h1>
+          
     
 <select className="form-select">
     
@@ -50,38 +46,40 @@ const Navbars= () => {
 </select>
     </div>
         <div className=" dropdown d-flex ">
-    <a className="nav-link">
-            USD:
-          </a>
+    <h2>Usd:</h2>
+          
     
 <select className="form-select">
     
-    <option defaultValue = {" "}>Usd</option>
+    <option defaultValue = {" "}>USD</option>
     <option defaultValue= {" "}>name</option>
     <option defaultValue= {" "}>email</option>
     <option defaultValue= {" "}>age</option>
 </select>
     </div>
-</Stack>
 
+    
+</Nav>
+</div>
 
-</nav>
+    
 
     <nav className="navbar p-3 all-nav text-white">
   
-    <a className="navbar-brand electronics" href="#"><span><LuDisc3 /></span>Cilicon</a>
+    <a className="col-md-2 navbar-brand text-white" href="#"><span><LuDisc3 /></span>Cilicon</a>
    
     
   
-    <form className="d-flex" role="search">
+    <form className=" col-md-6 d-flex" role="search">
       <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success" type="submit">Search</button>
+      <button className="btn btn-outline-success bg-info" type="submit">Search</button>
     </form>
   
-<Stack  className ="electronics" direction="horizontal" gap={4}>
+<Nav  className =" col-md-3 d-flex ">
+<Stack direction="horizontal" gap={3}>
 <Link to = '/cart'>
   <Button style={{width: "3rem", height: "3rem", position:"relative" }} variant="outline-secondary" className="rounded-circle">
-    <span><MdShoppingCart /></span>
+    <span style={{color: "white"}}><MdShoppingCart /></span>
     <div className="rounded-circle bg-danger d-flex justify-content-center" style={{color:"white", width: "1.5rem",
      height:"1.5rem", position:"absolute", right:0, bottom:0, transform:"translate(25%, 25%)" }}>5
 
@@ -93,27 +91,12 @@ const Navbars= () => {
   <span><FaUsersGear /></span>
   
   <span><FaUser/></span>
-  
   </Stack>
+  </Nav>
 
 
 
   
-  <Stack direction="horizontal" gap={4} className= " electronics bg-secondary p-1">
-  <Link to = '/cart'>
-        <span ><FaShoppingBasket/></span>
-    </Link>
-    <Link to = '/user'>
-        <span><FaUser/></span>
-    </Link>
-    
-
-    
-        <span><FaGem/></span>
-    
-
-  </Stack>
-   
     
 
 </nav>
@@ -122,7 +105,7 @@ const Navbars= () => {
 
 <Nav className="navbar bg-white p-2 me-5">
              
-            <Stack direction ="horizontal" gap={3}>
+            <Nav className="d-flex justify-content-between">
             <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             All Category
@@ -138,7 +121,7 @@ const Navbars= () => {
         <h3><span><MdOutlinePhoneCallback />customer support</span></h3>
         <h4><span><FaHotel/>need help</span></h4>
         
-            </Stack>
+            </Nav>
             <span> <MdOutlineAddIcCall />09154040945</span>
             
 
